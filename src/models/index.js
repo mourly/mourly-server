0a7ceccb35d30e3d956683fad2d6e58427ed13c4
exiondew <exiondew@gmail.com>
2024-12-18 00:09:52 +0300

    refactor: centralize model exports
    - Centralized the export of User, BlockedUsers, and UserFriends models from a single file.
    - Simplified imports for models throughout the application.

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+const User = require("./UserModel");
+const BlockedUsers = require("./BlockedUsersModel");
+const UserFriends = require("./UserFriendsModel");
+
+module.exports = {
+  BlockedUsers,
+  UserFriends,
+  User,
+};
