62276792c1f25e9e1192ecaf18b9463ddb2117e4
exiondew <exiondew@gmail.com>
2024-12-20 18:03:13 +0300

    refactor: adjust middleware and server start order in index.js - routes middleware is now invoked after database connection and sync - server.listen starts after async operations complete

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", routes);
+
 (async () => {
   await connectDB();
   await syncDB();
+
+  server.listen(port, () =>
+    console.log(`Sunucu ${port} portundan dinleniyor...`)
+  );
 })();
-
-app.use("/", routes);
-
-server.listen(port, () =>
-  console.log(`Sunucu ${port} portundan dinleniyor...`)
-);
