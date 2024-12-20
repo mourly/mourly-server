a9e97f83476533951ba7e2618618c68be298d130
exiondew <exiondew@gmail.com>
2024-12-20 18:07:04 +0300

    feat: added new validation rules to authValidations - dateValidation added for age verification - termValidation added to check term acceptance - tokenValidation added for JWT validation - registerValidation now includes all validations - loginValidation defined for login operations - fixed regex issue in usernameValidation

--- src/validations/authValidations.js
+++ src/validations/authValidations.js
@@ -1,4 +1,5 @@
 const { body } = require("express-validator");
+const { calculateAge } = require("../utils/date");
 
 // ----- USERNAME VALIDATION ----- //
 const usernameValidation = [
@@ -10,7 +11,7 @@ const usernameValidation = [
     .withMessage("auth_100")
     .matches(/^[a-z0-9_]+$/)
     .withMessage("auth_102")
-    .matches(/_$/)
+    .matches(/^(?!.*_$)/)
     .withMessage("auth_103"),
 ];
 
@@ -44,16 +45,46 @@ const passwordValidation = [
     .withMessage("auth_125"),
 ];
 
+// ----- DATE VALIDATION ----- //
+const dateValidation = [
+  body("date")
+    .isISO8601()
+    .withMessage("auth_5")
+    .custom((value) => {
+      const validAge = calculateAge(value);
+      if (validAge >= 18) return true;
+
+      throw new Error("auth_2");
+    }),
+];
+
+// ----- TERM VALIDATION ----- //
+const termValidation = [body("term").equals("true").withMessage("auth_3")];
+
+// ----- TOKEN VALIDATION ----- //
+const tokenValidation = [body("token").isJWT().withMessage("auth_4")];
+
 // ----- REGISTER VALIDATION ----- //
 const registerValidation = [
   ...usernameValidation,
   ...emailValidation,
   ...passwordValidation,
+  ...termValidation,
+  ...dateValidation,
 ];
 
+// ----- LOGIN VALIDATION ----- //
+
+const loginValidation = [...emailValidation, ...passwordValidation];
+
+// ----- Exports ----- //
 module.exports = {
   usernameValidation,
   emailValidation,
   passwordValidation,
+  dateValidation,
+  termValidation,
+  tokenValidation,
   registerValidation,
+  loginValidation,
 };
