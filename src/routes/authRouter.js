2988f3f824d64c17e5a488ab4f6b9e0e065b960f
exiondew <exiondew@gmail.com>
2024-12-20 18:32:21 +0300

    Enhance auth routes with rate limiting, login, and refresh token support
    - Added rate limiting for register and login routes.
    - Introduced a new route for token refresh with rate limiting.
    - Included validation middleware for register, login, and refresh token endpoints.

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,12 +1,52 @@
 const { Router } = require("express");
-const { registerController } = require("../controllers/authControllers");
+const {
+  registerController,
+  loginController,
+  refreshTokenController,
+} = require("../controllers/authControllers");
 
-const { validate, registerValidation } = require("../validations");
+const {
+  validate,
+  registerValidation,
+  loginValidation,
+  tokenValidation,
+} = require("../validations");
+const createRateLimiter = require("../middlewares/rateLimitMiddleware");
 
 // xxx/auth
 const router = Router();
 
+const authRateLimiter = createRateLimiter({ ms: 1000 * 60, max: 15 });
+const tokenRateLimiter = createRateLimiter({
+  max: 3,
+  ms: 60 * 60 * 1000,
+});
+
 // xxx/auth/register [POST]
-router.post("/register", registerValidation, validate, registerController);
+router.post(
+  "/register",
+  authRateLimiter,
+  registerValidation,
+  validate,
+  registerController
+);
+
+// xxx/auth/register [POST]
+router.post(
+  "/login",
+  authRateLimiter,
+  loginValidation,
+  validate,
+  loginController
+);
+
+// xxx/auth/refresh-token [POST]
+router.post(
+  "/refresh-token",
+  tokenRateLimiter,
+  tokenValidation,
+  validate,
+  refreshTokenController
+);
 
 module.exports = router;
