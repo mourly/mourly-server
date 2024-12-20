32689cd620019dbf6573ca0c1b7c2388e522eb71
exiondew <exiondew@gmail.com>
2024-12-20 18:05:32 +0300

    feat: add new error types to ERROR_TYPES constant - added authentication_err and authorization_err error types - added token_exp_err for token expiration scenarios

--- src/constants/errors/types.js
+++ src/constants/errors/types.js
@@ -5,6 +5,9 @@ const ERROR_TYPES = {
   user_err: "USER_ERROR",
   timeout_err: "TIMEOUT_ERROR",
   rate_limit_err: "RATE_LIMIT_ERROR",
+  authentication_err: "AUTHENTICATION_ERROR",
+  authorization_err: "AUTHORIZATION_ERROR",
+  token_exp_err: "TOKEN_EXPIRED_ERROR",
 };
 
 module.exports = ERROR_TYPES;
